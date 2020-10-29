4da80142d0d8a8e7248825abc4acc65764562788
Steve Meyer <stephen.meyer@wisc.edu>
2020-10-28 21:24:46 -0500

    Change shifter to use attributes instead of internal variables

--- source/projects/weft.shifter/weft.shifter.cpp
+++ source/projects/weft.shifter/weft.shifter.cpp
@@ -16,60 +16,52 @@ public:
     MIN_AUTHOR		{"Steve Meyer"};
     MIN_RELATED		{"zl"};
 
-    inlet<>  input_l { this, "(bang) send shifted sequence; (list) set the sequence" };
-    inlet<>  input_r { this, "(list) set the sequence shifts" };
-    outlet<> output	{ this, "(list) the transformed list, shifted where applicable" };
+
+    inlet<>  input  { this, "(bang) output shifted sequence." };
+    outlet<> output { this, "(list) the transformed sequence." };
 
 
-    message<threadsafe::yes> bang { this, "bang", "Send out the shifted sequence",
-        MIN_FUNCTION {
-            lock  lock {m_mutex};
-            atoms sequence_copy = m_sequence;
-            lock.unlock();
-
-            if (m_shifts.size() > 0) {
-                for (int i = 0; i < sequence_copy.size(); i++) {
-                    int step = sequence_copy[i];
-                    if (step != 0) {
-                        int shift_step = m_shifts[i % m_shifts.size()];
-                        sequence_copy[i] = atom(step + shift_step);
-                    }
-                }
-            }
-
-            output.send(sequence_copy);
-            return {};
-        }
+    attribute< vector<int> > sequence { this, "sequence", {0}, description {"The primary sequence to shift."},
+        setter { MIN_FUNCTION {
+            if (args.size() == 0 || !only_ints(args))
+                return this->sequence;
+            else
+                return args;
+        }}
     };
 
 
-    message<threadsafe::yes> list { this, "list", "In left inlet, sets the primary sequence; in right inlet, sets the shift sequence that transforms the primary sequence.",
-        MIN_FUNCTION {
-
-            if (only_ints(args))
-            {
-                lock lock {m_mutex};
-                if (inlet == 0) {
-                    m_sequence.clear();
-                    m_sequence.reserve(m_sequence.size() + args.size());
-                    m_sequence.insert(m_sequence.end(), args.begin(), args.end());
-                } else {
-                    m_shifts.clear();
-                    m_shifts.reserve(m_shifts.size() + args.size());
-                    m_shifts.insert(m_shifts.end(), args.begin(), args.end());
-                }
-            }
+    attribute< vector<int> > shift_pattern { this, "shift_pattern", {0}, description {"The shift pattern used to transform the primary sequence."},
+        setter { MIN_FUNCTION {
+            if (args.size() == 0 || !only_ints(args))
+                return this->shift_pattern;
             else
-                cerr << "Cannot process list with a non-integer element." << endl;
+                return args;
+        }}
+    };
 
+
+    message<> bang { this, "bang", "Send out the shifted sequence.",
+        MIN_FUNCTION {
+            lock  lock {m_mutex};
+
+            atoms shifted_seq;
+            auto seq    = from_atoms<std::vector<int>>(this->sequence);
+            auto shifts = from_atoms<std::vector<int>>(this->shift_pattern);
+            for (int i = 0; i < seq.size(); i++)
+                if (seq[i] == 0)
+                    shifted_seq.push_back(seq[i]);
+                else
+                    shifted_seq.push_back(seq[i] + shifts[i % shifts.size()]);
+
+            lock.unlock();
+            output.send(shifted_seq);
             return {};
         }
     };
 
 
 private:
-    atoms m_sequence;
-    atoms m_shifts;
     mutex m_mutex;
 };
 
--- source/projects/weft.shifter/weft.shifter_test.cpp
+++ source/projects/weft.shifter/weft.shifter_test.cpp
@@ -6,8 +6,6 @@
 #include "c74_min_unittest.h"     // required unit test header
 #include "weft.shifter.cpp"    // need the source of our object so that we can access it
 
-// Unit tests are written using the Catch framework as described at
-// https://github.com/philsquared/Catch/blob/master/docs/tutorial.md
 
 SCENARIO("object produces correct output") {
     ext_main(nullptr);    // every unit test must call ext_main() once to configure the class
@@ -23,12 +21,14 @@ SCENARIO("object produces correct output") {
             sequence.push_back(1);
             sequence.push_back("string");
             sequence.push_back(2);
-            my_object.list(sequence);
+            my_object.sequence = sequence;
             my_object.bang();
 
             THEN("it does not update the sequence") {
                 auto& output = *c74::max::object_getoutput(my_object, 0);
-                REQUIRE(output.size() == 0);
+                atoms expected = {0};
+                REQUIRE(output.size() == 1);
+                REQUIRE(output[0] == expected);
             }
         }
 
@@ -38,12 +38,14 @@ SCENARIO("object produces correct output") {
             sequence.push_back(1);
             sequence.push_back(2);
             sequence.push_back(3.1456);
-            my_object.list(sequence);
+            my_object.sequence = sequence;
             my_object.bang();
 
             THEN("it does not update the sequence") {
                 auto& output = *c74::max::object_getoutput(my_object, 0);
-                REQUIRE(output.size() == 0);
+                atoms expected = {0};
+                REQUIRE(output.size() == 1);
+                REQUIRE(output[0] == expected);
             }
         }
     }
@@ -54,92 +56,112 @@ SCENARIO("object produces correct output") {
         shifter&             my_object = an_instance;
 
         // Seed the initial sequence
-        my_object.list({ 1, 2, 3, 4 });
+        atoms sequence = {1, 2, 3, 4};
+        my_object.sequence = sequence;
 
         WHEN("it is banged") {
             my_object.bang();
+
             THEN("the sequence is returned") {
                 auto& output = *c74::max::object_getoutput(my_object, 0);
-                REQUIRE(output.size() == 1);
-                REQUIRE(output[0].size() == 4);
                 atoms expected = { 1, 2, 3, 4 };
+                REQUIRE(output.size() == 1);
+                REQUIRE(output[0] == expected);
+            }
+        }
+
+        WHEN("it is given an invalid sequence") {
+            atoms sequence;
+            sequence.push_back(1);
+            sequence.push_back(2);
+            sequence.push_back(3.1456);
+            my_object.sequence = sequence;
+            my_object.bang();
+
+            THEN("it does not update the current, valid sequence") {
+                auto& output = *c74::max::object_getoutput(my_object, 0);
+                atoms expected = { 1, 2, 3, 4 };
+                REQUIRE(output.size() == 1);
                 REQUIRE(output[0] == expected);
             }
         }
 
         WHEN("a shift sequence is provided and it is banged") {
             atoms shift_seq = { 1, 0 };
-            my_object.list(shift_seq, 1);
+            my_object.shift_pattern = shift_seq;
             my_object.bang();
 
             THEN("the returned sequence has shifts applied") {
                 auto& output = *c74::max::object_getoutput(my_object, 0);
-                REQUIRE(output.size() == 1);
-                REQUIRE(output[0].size() == 4);
                 atoms expected = { 2, 2, 4, 4 };
+                REQUIRE(output.size() == 1);
                 REQUIRE(output[0] == expected);
             }
 
             AND_WHEN("a new shift sequence provided and it is banged") {
                 atoms shift_seq = { 0, 1 };
-                my_object.list(shift_seq, 1);
+                my_object.shift_pattern = shift_seq;
                 my_object.bang();
 
                 THEN("the returned sequence has shifts applied") {
                     auto& output = *c74::max::object_getoutput(my_object, 0);
-                    REQUIRE(output.size() == 2);
-                    REQUIRE(output[1].size() == 4);
                     atoms expected = { 1, 3, 3, 5 };
+                    REQUIRE(output.size() == 2);
                     REQUIRE(output[1] == expected);
                 }
             }
         }
 
         WHEN("it is given a list again and it is banged") {
-            my_object.list({4, 3, 2, 1});
+            atoms sequence = {4, 3, 2, 1};
+            my_object.sequence = sequence;
             my_object.bang();
+
             THEN("the new list replaces the old list and is returned") {
                 auto& output = *c74::max::object_getoutput(my_object, 0);
-                REQUIRE(output.size() == 1);
-                REQUIRE(output[0].size() == 4);
                 atoms expected = {4, 3, 2, 1};
+                REQUIRE(output.size() == 1);
                 REQUIRE(output[0] == expected);
             }
         }
 
         WHEN("it is given a shift sequence that is not a multiple of the sequence") {
-            my_object.list({1, 0, 1}, 1);
+            atoms shift_seq = {1, 0, 1};
+            my_object.shift_pattern = shift_seq;
             my_object.bang();
+
             THEN("the resulting transformation uses a modulo/wrap around model") {
                 auto& output = *c74::max::object_getoutput(my_object, 0);
-                REQUIRE(output.size() == 1);
-                REQUIRE(output[0].size() == 4);
                 atoms expected = {2, 2, 4, 5};
+                REQUIRE(output.size() == 1);
                 REQUIRE(output[0] == expected);
             }
         }
 
         WHEN("it is given a shift sequence that is longer than the primary sequence") {
-            my_object.list({1, 0, 1, 0, 1}, 1);
+            atoms shift_seq = {1, 0, 1, 0, 1};
+            my_object.shift_pattern = shift_seq;
             my_object.bang();
+
             THEN("the resulting transformation uses a modulo/wrap around model") {
                 auto& output = *c74::max::object_getoutput(my_object, 0);
-                REQUIRE(output.size() == 1);
-                REQUIRE(output[0].size() == 4);
                 atoms expected = {2, 2, 4, 4};
+                REQUIRE(output.size() == 1);
                 REQUIRE(output[0] == expected);
             }
         }
 
         WHEN("the primary sequences has 0-value rest steps") {
-            my_object.list({1, 0, 0, 0});
-            my_object.list({1, 0}, 1);
+            atoms sequence  = {1, 0, 0, 0};
+            atoms shift_seq = {1, 0};
+            my_object.sequence = sequence;
+            my_object.shift_pattern = shift_seq;
             my_object.bang();
+
             THEN("the resulting transformation does not modify the rests") {
                 auto& output = *c74::max::object_getoutput(my_object, 0);
-                REQUIRE(output.size() == 1);
-                REQUIRE(output[0].size() == 4);
                 atoms expected = {2, 0, 0, 0};
+                REQUIRE(output.size() == 1);
                 REQUIRE(output[0] == expected);
             }
         }
