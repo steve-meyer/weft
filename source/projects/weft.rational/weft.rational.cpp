aa18fcc343eb9e048c2b0b81bf00a7c403fd6822
Steve Meyer <stephen.meyer@wisc.edu>
2020-12-02 22:11:48 -0600

    First work on rational melodies (number XI)

---
+++ source/projects/weft.rational/weft.rational.cpp
@@ -0,0 +1,69 @@
+/// @file
+///	@ingroup   weft
+///	@copyright Copyright 2020 Stephen Meyer. All rights reserved.
+///	@licence	     Use of this source code is governed by the MIT License found in the License.md file.
+
+#include "c74_min.h"
+#include "../weft.shared/weft.h"
+
+using namespace c74::min;
+
+
+class rational : public object<rational> {
+public:
+    MIN_DESCRIPTION {"Transform a sequence by rational melody algorithms."};
+    MIN_TAGS        {"sequences, transformations"};
+    MIN_AUTHOR      {"Steve Meyer"};
+    MIN_RELATED     {"zl"};
+
+
+    inlet<>  input  { this, "(bang) send out transformed sequence" };
+    outlet<> output { this, "(list) the transformed sequence as a list." };
+    
+    
+    attribute<symbol> melody { this, "melody", "XI", description {"The rational melody number (in roman numerals)."} };
+
+
+    attribute< vector<int> > sequence { this, "sequence", {0}, description {"The primary sequence to transform."},
+        setter { MIN_FUNCTION {
+            if (args.size() == 0 || !only_ints(args))
+                return this->sequence;
+            else
+                return args;
+        }}
+    };
+
+
+    message<> bang { this, "bang", "Send out the transformed sequence with rational melody algorithm applied.",
+        MIN_FUNCTION {
+            lock  lock {m_mutex};
+
+            atoms transformed_seq;
+            vector<int> seq   = from_atoms<std::vector<int>>(this->sequence);
+            int segment_length = seq.size() / 2 + 1;
+            
+            // Do that dance Paula sang about...
+            for (int segment = 0; segment < seq.size(); segment++) {
+
+                // Take two steps forward,
+                for (int index = 0; index < segment_length; index++)
+                    transformed_seq.push_back( seq[(index + segment) % seq.size()] );
+                
+                // Take one step back.
+                for (int index = 0; index < segment_length - 1; index++)
+                    transformed_seq.push_back( transformed_seq[transformed_seq.size() - 1 - index - index] );
+            }
+
+            lock.unlock();
+            output.send(transformed_seq);
+            return {};
+        }
+    };
+
+
+private:
+    mutex m_mutex;
+};
+
+
+MIN_EXTERNAL(rational);
---
+++ source/projects/weft.rational/weft.rational_test.cpp
@@ -0,0 +1,84 @@
+/// @file
+/// @ingroup   weft
+/// @copyright Copyright 2020 Stephen Meyer. All rights reserved.
+/// @license        Use of this source code is governed by the MIT License found in the License.md file.
+
+#include "c74_min_unittest.h"  // required unit test header
+#include "weft.rational.cpp"   // need the source of our object so that we can access it
+
+
+SCENARIO("Object produces correct output") {
+    ext_main(nullptr);    // every unit test must call ext_main() once to configure the class
+
+     GIVEN("An instance of weft.rational with a sequence containing an even number of steps") {
+    
+         test_wrapper<rational> an_instance;
+         rational&             my_object = an_instance;
+         atoms sequence = {1, 2, 3, 4};
+         my_object.sequence = sequence;
+    
+//         WHEN("it is given a gate pattern with non-integers and it is banged") {
+//             atoms gates;
+//             gates.push_back(1);
+//             gates.push_back("string");
+//             gates.push_back(0);
+//             my_object.gates_pattern = gates;
+//             my_object.bang();
+//
+//             THEN("the gates are not stored and the original transformation is returned") {
+//                 auto& output = *c74::max::object_getoutput(my_object, 0);
+//                 atoms expected = {1, 1, 5, 5, 6, 6};
+//                 REQUIRE(output.size() == 1);
+//                 REQUIRE(output[0] == expected);
+//             }
+//         }
+
+         WHEN("it is given melody number XI and it is banged") {
+             atom melody = "XI";
+             my_object.melody = melody;
+             my_object.bang();
+    
+             THEN("the transformed sequence is returned") {
+                 auto& output = *c74::max::object_getoutput(my_object, 0);
+                 atoms expected = {
+                     1, 2, 3, 3, 2,
+                     2, 3, 4, 4, 3,
+                     3, 4, 1, 1, 4,
+                     4, 1, 2, 2, 1
+                 };
+                 REQUIRE(output.size() == 1);
+                 REQUIRE(output[0] == expected);
+             }
+         }
+    
+    //     WHEN("the sequence that is not a multiple of the gates pattern") {
+    //         atoms sequence = {1, 1, 5, 5, 7};
+    //         atoms gates = {1, 0, 0};
+    //         my_object.sequence = sequence;
+    //         my_object.gates_pattern = gates;
+    //         my_object.bang();
+    //
+    //         THEN("the gates are applied to the steps in the sequence") {
+    //             auto& output = *c74::max::object_getoutput(my_object, 0);
+    //             atoms expected = {1, 0, 0, 5, 0};
+    //             REQUIRE(output.size() == 1);
+    //             REQUIRE(output[0] == expected);
+    //         }
+    //     }
+    //
+    //     WHEN("the sequence is shorter than the gates pattern") {
+    //         atoms sequence = {1, 5};
+    //         atoms gates = {1, 0, 0};
+    //         my_object.sequence = sequence;
+    //         my_object.gates_pattern = gates;
+    //         my_object.bang();
+    //
+    //         THEN("the gates are applied to the steps in the sequence") {
+    //             auto& output = *c74::max::object_getoutput(my_object, 0);
+    //             atoms expected = {1, 0};
+    //             REQUIRE(output.size() == 1);
+    //             REQUIRE(output[0] == expected);
+    //         }
+    //     }
+     }
+}
